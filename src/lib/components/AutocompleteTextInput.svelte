cda4982519680e6ab40999e0a56bc230718f015e
Nikita Galaiko <nikita@galaiko.rocks>
2023-01-30 10:45:35 +0100

    autocomplete on tab

--- src/lib/components/AutocompleteTextInput.svelte
+++ src/lib/components/AutocompleteTextInput.svelte
@@ -5,24 +5,57 @@
     export let value: string = "";
     export let required: boolean = false;
 
+    let input: HTMLInputElement;
+
     export let sources: string[] = [];
+
     $: suggestion = mostCommon(
         sources.filter((source) => source.startsWith(value))
     )?.slice(value.length);
+
+    const keydown = (
+        node: HTMLElement,
+        map: Record<string, (e: KeyboardEvent) => void>
+    ) => {
+        const listener = (e: KeyboardEvent) => {
+            if (map[e.key]) map[e.key](e);
+        };
+        node.addEventListener("keydown", listener);
+        return {
+            destroy: () => {
+                node.removeEventListener("keydown", listener);
+            },
+        };
+    };
+
+    const complete = (e: KeyboardEvent) => {
+        if (suggestion) {
+            e.preventDefault();
+            value += suggestion;
+        }
+    };
 </script>
 
-<div class="flex flex-1">
+<div class="flex flex-1 relative">
     <input
+        use:keydown={{ Tab: complete }}
         type="text"
         {name}
         bind:value
-        size={value.length}
         placeholder={suggestion}
-        class="hover:outline-none focus:outline-none"
-        class:flex-1={!value}
+        class="flex-1 hover:outline-none focus:outline-none"
         {required}
+        bind:this={input}
     />
     {#if value && suggestion}
-        <span class="flex-1" style:color="darkgrey">{suggestion}</span>
+        <button
+            type="button"
+            on:click={() => input.focus()}
+            class="cursor-text flex-1 absolute top-0"
+            style:color="darkgrey"
+            style:left="{value.length}ch"
+        >
+            {suggestion}
+        </button>
     {/if}
 </div>

b8d19fb6c1b82a514ca6b15b62c688494ddcb9ef
Nikita Galaiko <nikita@galaiko.rocks>
2023-01-31 13:56:29 +0100

    collapse account names

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,12 +25,43 @@
                 )
                 .sort((a, b) => b.tindex - a.tindex)
     );
+
+    const collapseAccount = (e: HTMLElement) => {
+        const collapse = (e: HTMLElement) => {
+            if (e.textContent === null) return;
+
+            if (e.dataset["value"] !== undefined) {
+                e.textContent = e.dataset["value"];
+            } else {
+                e.dataset["value"] = e.textContent;
+            }
+
+            while (e.offsetWidth < e.scrollWidth) {
+                const parts: string[] = e.textContent.split(":");
+                const firstLongPartIndex = parts.findIndex((p) => p.length > 1);
+                if (firstLongPartIndex === -1) return;
+                e.textContent = [
+                    ...parts.slice(0, firstLongPartIndex),
+                    parts[firstLongPartIndex][0],
+                    ...parts.slice(firstLongPartIndex + 1),
+                ].join(":");
+            }
+        };
+
+        collapse(e);
+
+        const onResize = () => collapse(e);
+        window.addEventListener("resize", onResize);
+        return {
+            destroy: () => window.removeEventListener("resize", onResize),
+        };
+    };
 </script>
 
 {#await $fetchTransactions}
     loading...
 {:then _}
-    <ul class="flex flex-col gap-2">
+    <ul class="flex flex-col gap-4">
         <li class="flex flex-1">
             <TransactionForm transactions={displayTransactions} />
         </li>
@@ -51,26 +82,22 @@
                         </time>
                         <span>{transaction.tdescription}</span>
                     </figcaption>
-                    <ul class="flex flex-col gap-2">
+                    <ul class="ml-[4ch] flex flex-col gap-1">
                         {#each transaction.tpostings as posting, i}
-                            {@const amount =
-                                posting.pamount.length > 0
-                                    ? Amount.format(posting.pamount[0])
-                                    : null}
+                            {@const amount = Amount.format(posting.pamount[0])}
                             {@const assertion = assertions[i]}
                             <li class="flex gap-[2ch]">
                                 <span
+                                    use:collapseAccount
                                     title={posting.paccount}
                                     class="flex-1 text-ellipsis overflow-hidden whitespace-pre"
                                 >
                                     {posting.paccount}
                                 </span>
 
-                                {#if amount}
-                                    <span class="whitespace-nowrap">
-                                        {amount}
-                                    </span>
-                                {/if}
+                                <span class="whitespace-nowrap">
+                                    {amount}
+                                </span>
 
                                 {#if maxAssetionLength > 0}
                                     <span

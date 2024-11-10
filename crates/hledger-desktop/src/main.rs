3452236d8caac23cf7ba97c97259cc319a104cd9
Nikita Galaiko <nikita@galaiko.rocks>
2024-11-10 14:33:27 +0100

    hledger-desktop: load journal

--- crates/hledger-desktop/src/main.rs
+++ crates/hledger-desktop/src/main.rs
@@ -1,43 +1,23 @@
-use iced::widget::text;
-use iced::{Center, Element, Fill, Task as Command};
+mod app;
+mod journal;
+
+use app::HledgerDesktop;
+use tracing_subscriber::util::SubscriberInitExt;
 
 #[allow(clippy::missing_errors_doc)]
 pub fn main() -> iced::Result {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt::fmt()
+        .with_span_events(
+            tracing_subscriber::fmt::format::FmtSpan::NEW
+                | tracing_subscriber::fmt::format::FmtSpan::CLOSE,
+        )
+        .finish()
+        .init();
 
     iced::application(
         HledgerDesktop::title,
         HledgerDesktop::update,
         HledgerDesktop::view,
     )
-    .window_size((500.0, 800.0))
     .run()
-}
-
-#[derive(Debug, Default)]
-struct HledgerDesktop {}
-
-#[derive(Debug, Clone, Copy)]
-enum Message {}
-
-impl HledgerDesktop {
-    #[allow(clippy::unused_self)]
-    fn title(&self) -> String {
-        String::from("hledger-deskop")
-    }
-
-    #[allow(clippy::unused_self)]
-    fn update(&mut self, _message: Message) -> Command<Message> {
-        Command::none()
-    }
-
-    #[allow(clippy::unused_self)]
-    fn view(&self) -> Element<Message> {
-        text("hello world")
-            .width(Fill)
-            .size(100)
-            .align_x(Center)
-            .align_y(Center)
-            .into()
-    }
 }

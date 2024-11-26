4ed3a0aa124e99e9698bff2f85ecad9133b25d86
Nikita Galaiko <nikita@galaiko.rocks>
2024-11-26 21:27:40 +0100

    iced-virtual-list: init

---
+++ crates/iced-virtual-list/README.rs
@@ -0,0 +1,5 @@
+# iced-virtual-list
+
+Implementation of a virtual list widget for [iced](https://iced.rs).
+
+Taken [from here](https://github.com/iced-rs/iced/blob/feature/list-widget-reloaded/widget/src/list.rs).

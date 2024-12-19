7a7776c3784465e8772ac881484e20339beb91fc
Nikita Galaiko <nikita@galaiko.rocks>
2024-12-19 17:56:05 +0100

    hledger-parser-ui: use account_name.display

--- crates/hledger-desktop-ui/src/app/window/file.rs
+++ crates/hledger-desktop-ui/src/app/window/file.rs
@@ -118,7 +118,7 @@ fn transaction_ui(ui: &mut Ui, transaction: &hledger_journal::Transaction) {
 
 fn posting_ui(ui: &mut Ui, posting: &hledger_journal::Posting) {
     ui.columns(2, |columns| {
-        columns[0].label(RichText::new(posting.account_name.join(":")).monospace());
+        columns[0].label(RichText::new(posting.account_name.to_string()).monospace());
         if let Some(amount) = &posting.amount {
             columns[1].with_layout(Layout::right_to_left(Align::Min), |ui| {
                 ui.label(&amount.commodity);

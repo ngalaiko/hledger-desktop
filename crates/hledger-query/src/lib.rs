83c92c5be3dd4ca96f814254df3e590d2d1cf3f6
Nikita Galaiko <nikita@galaiko.rocks>
2024-12-27 13:34:56 +0100

    hledger-query: all transaction filters

--- crates/hledger-query/src/lib.rs
+++ crates/hledger-query/src/lib.rs
@@ -1,18 +1,16 @@
 pub use hledger_parser::{ParseError, Posting, Transaction};
 
-type TransactionFilter = Box<dyn for<'a> Fn(&'a Transaction) -> bool>;
-
 type PostingFilter = Box<dyn for<'a> Fn(&'a Posting) -> bool>;
 
 pub struct Query {
-    transaction_filter: TransactionFilter,
+    filter: transaction::Filter,
     posting_filter: PostingFilter,
 }
 
 impl Default for Query {
     fn default() -> Self {
         Self {
-            transaction_filter: Box::new(|_| true),
+            filter: Box::new(|_| true),
             posting_filter: Box::new(|_| true),
         }
     }
@@ -30,23 +28,20 @@ impl Query {
     #[allow(clippy::missing_errors_doc)]
     pub fn parse(query: &str) -> Result<Query, Error> {
         let terms = hledger_parser::parse_query(query).map_err(Error::Parse)?;
-        let transaction_filters = terms
-            .iter()
-            .map(to_transaction_filter)
-            .collect::<Result<Vec<_>, _>>()?;
+        let transaction_filters = terms.iter().map(to_filter).collect::<Result<Vec<_>, _>>()?;
         let posting_filters = terms
             .iter()
             .map(to_posting_filter)
             .collect::<Result<Vec<_>, _>>()?;
         Ok(Self {
-            transaction_filter: Box::new(move |tx| transaction_filters.iter().all(|f| f(tx))),
+            filter: Box::new(move |tx| transaction_filters.iter().all(|f| f(tx))),
             posting_filter: Box::new(move |posting| posting_filters.iter().all(|f| f(posting))),
         })
     }
 
     #[must_use]
     pub fn filter(&self, tx: &Transaction) -> Option<Transaction> {
-        if !(self.transaction_filter)(tx) {
+        if !(self.filter)(tx) {
             return None;
         }
 
@@ -67,24 +62,24 @@ impl Query {
     }
 }
 
-fn to_transaction_filter(term: &hledger_parser::Term) -> Result<TransactionFilter, Error> {
-    let is_not = term.is_not;
-    match &term.condition {
-        hledger_parser::Condition::Description(r) => {
-            let r = regex::RegexBuilder::new(r)
-                .case_insensitive(true)
-                .build()
-                .map_err(Error::Regex)?;
-            Ok(Box::new(move |tx| {
-                let is_match = if let Some(note) = &tx.note {
-                    r.is_match(&format!("{} | {note}", tx.payee))
-                } else {
-                    r.is_match(&tx.payee)
-                };
-                (is_match && !is_not) || (!is_match && is_not)
-            }))
+fn to_filter(term: &hledger_parser::Term) -> Result<transaction::Filter, Error> {
+    let filter = match &term.condition {
+        hledger_parser::Condition::Account(_)
+        | hledger_parser::Condition::Currency(_)
+        | hledger_parser::Condition::Amount(_) => Ok(transaction::always_true_filter()),
+        hledger_parser::Condition::Code(query) => transaction::code_filter(query),
+        hledger_parser::Condition::Description(query) => transaction::description_filter(query),
+        hledger_parser::Condition::Note(query) => transaction::note_filter(query),
+        hledger_parser::Condition::Payee(query) => transaction::payee_filter(query),
+        hledger_parser::Condition::Date(period) => Ok(transaction::date_filter(period)),
+        hledger_parser::Condition::Status(status) => {
+            Ok(transaction::status_filter(status.as_ref()))
         }
-        _ => Ok(Box::new(|_| true)),
+    }?;
+    if term.is_not {
+        Ok(transaction::not_filter(filter))
+    } else {
+        Ok(filter)
     }
 }
 
@@ -104,5 +99,86 @@ fn to_posting_filter(term: &hledger_parser::Term) -> Result<PostingFilter, Error
             }))
         }
         _ => Ok(Box::new(|_| true)),
+    }
+}
+
+mod transaction {
+    use hledger_parser::{Period, Status};
+
+    use super::{Error, Transaction};
+
+    pub type Filter = Box<dyn for<'a> Fn(&'a Transaction) -> bool>;
+
+    pub fn not_filter(filter: Filter) -> Filter {
+        Box::new(move |tx| !filter(tx))
+    }
+
+    pub fn description_filter(query: &str) -> Result<Filter, Error> {
+        let r = regex::RegexBuilder::new(query)
+            .case_insensitive(true)
+            .build()
+            .map_err(Error::Regex)?;
+        Ok(Box::new(move |tx| {
+            if let Some(note) = &tx.note {
+                r.is_match(&format!("{} | {note}", tx.payee))
+            } else {
+                r.is_match(&tx.payee)
+            }
+        }))
+    }
+
+    pub fn date_filter(date: &Period) -> Filter {
+        let begin = date.begin;
+        let end = date.end;
+        Box::new(move |tx| {
+            if let Some(begin) = begin {
+                if tx.date < begin {
+                    return false;
+                }
+            }
+            if let Some(end) = end {
+                if tx.date >= end {
+                    return false;
+                }
+            }
+            true
+        })
+    }
+
+    pub fn status_filter(status: Option<&Status>) -> Filter {
+        let status = status.cloned();
+        Box::new(move |tx| tx.status == status)
+    }
+
+    pub fn payee_filter(query: &str) -> Result<Filter, Error> {
+        let r = regex::RegexBuilder::new(query)
+            .case_insensitive(true)
+            .build()
+            .map_err(Error::Regex)?;
+        Ok(Box::new(move |tx| r.is_match(&tx.payee)))
+    }
+
+    pub fn note_filter(query: &str) -> Result<Filter, Error> {
+        let r = regex::RegexBuilder::new(query)
+            .case_insensitive(true)
+            .build()
+            .map_err(Error::Regex)?;
+        Ok(Box::new(move |tx| {
+            tx.note.as_ref().map_or(true, |note| r.is_match(note))
+        }))
+    }
+
+    pub fn always_true_filter() -> Filter {
+        Box::new(|_| true)
+    }
+
+    pub fn code_filter(query: &str) -> Result<Filter, Error> {
+        let r = regex::RegexBuilder::new(query)
+            .case_insensitive(true)
+            .build()
+            .map_err(Error::Regex)?;
+        Ok(Box::new(move |tx| {
+            tx.code.as_ref().map_or(true, |note| r.is_match(note))
+        }))
     }
 }
